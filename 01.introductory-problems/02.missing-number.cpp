df93919582ccf2bec5692b89fe6d42f85246916c
hivi6 <89378957+hivi6@users.noreply.github.com>
2025-02-24 01:01:34 +0530

    Add comment to 'missing number'

--- 01.introductory-problems/02.missing-number.cpp
+++ 01.introductory-problems/02.missing-number.cpp
@@ -1,3 +1,13 @@
+/**
+ * Solution 1:
+ * Add the numbers to a set, and iterate from 1 to n to check which number
+ * doesn't exists
+ * 
+ * Solution 2:
+ * Add the sum from 1 to n. Now subtract the input elements. What remaining
+ * will be the missing number
+ * Total sum from 1 to n is an arithmetic sum which is (n * (n + 1) / 2)
+*/
 #include <iostream>
 #include <vector>
 #include <numeric>
